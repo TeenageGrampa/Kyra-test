cc8b42a20bf9a97e0eac36500fe94e4300205a7e
TeenageGrampa <danielhvoigt@gmail.com>
2020-01-09 16:06:27 -0500

    finished but hit api limit working on stretch goals

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,22 +7,45 @@ const chanId = 'UCvO6uJUVJQ6SrATfsWR5_aA'
 class HomePage extends Component {
 
     state = {
-        videos: []
+        videos: [],
+        firstpage: true,
+        nextpage: ''
+    }
+
+    componentDidMount(){
+        this.getData()
+        setInterval(this.getData, 5000)
     }
     
-    async componentDidMount(){
-        const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${APIKEY}&channelId=${chanId}&part=snippet,id&order=date&maxResults=10`)
+    getData = async (pageNum) => {
+        const token = pageNum
+        if(this.state.firstpage === true){
+            const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${APIKEY}&channelId=${chanId}&part=snippet,id&order=date&maxResults=50`)
+        const ytjson = await res.json()
+        this.setState({
+            videos: ytjson.items,
+            nextpage: ytjson.nextPageToken
+        })
+        } else if(this.state.firstpage === false){
+            const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${APIKEY}&channelId=${chanId}&part=snippet,id&order=date&maxResults=50&pageToken=${token}`)
         const ytjson = await res.json()
         this.setState({
             videos: ytjson.items
         })
+        }
+    }
+
+    nextPage = () => {
+        this.setState({
+            firstpage: false
+        },() => this.getData(this.state.nextpage))
     }
 
     displayvideos = () => {
         if(this.state.videos){
             const videos = this.state.videos.map( video => 
-            <div>
-                <img src={video.snippet.thumbnails.default.url} />
+            <div className="box column is-2" style={{margin: 7, borderRadius: 10}}>
+                <img src={video.snippet.thumbnails.medium.url} />
                 <p>{video.snippet.title}</p>
             </div>)
             return videos
@@ -32,10 +55,15 @@ class HomePage extends Component {
     }
 
     render(){
-        console.log(this.state)
         return(
-            <div>
-                {this.displayvideos()}
+            <div className="container">
+                <div style={{marginTop: 10}}>
+                    <h1 className="title">PAQ's Channel</h1>
+                    <div className="columns is-multiline">
+                        {this.displayvideos()}
+                    </div>
+                    <button onClick={this.nextPage}>Next Page</button>
+                </div>
             </div>
         )
     }
